0b72fcd93993884cf31226e038435f46471508ba
Eric <eric.loureiro@gmail.com>
2022-03-16 09:11:16 +0000

    chore(project): initial setup

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  verbose: true,
+  setupFilesAfterEnv: ['<rootDir>/src/tests/setup.tsx'],
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.stories.tsx', '!node_modules/'],
+  coverageReporters: ['text'],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
+  // Remove errors when importing these type of files on tests
+  moduleNameMapper: {
+    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|css|less)$':
+      '<rootDir>/src/tests/setup.tsx',
+  },
+};
+
+export default config;
---
+++ src/tests/setup.tsx
@@ -0,0 +1,20 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect';
+import { cleanup } from '@testing-library/react';
+
+beforeAll(() => {
+    jest.setTimeout(10000);
+});
+
+afterEach(() => {
+    cleanup();
+});
+
+beforeEach(() => {
+    jest.clearAllMocks();
+    jest.resetAllMocks();
+});
